abeb7b551786f211249b244b766d0a81c97a60f6
xiaofeng <2241279088@qq.com>
2023-03-09 20:49:53 +0800

    first

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @version V1.0
+ * @BelongsProject: git-test
+ * @BelongsPackage: com.atguigu.git
+ * @ClassName GitTest
+ * @Author:天道酬勤
+ * @Date: 2023/3/9 20:41
+ * @Description: TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
